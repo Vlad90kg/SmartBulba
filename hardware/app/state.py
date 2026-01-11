c3c0462a58b3088ea8ebebf548e5534b78d9a37f
vladikslon <vladbrovi@gmail.com>
2026-01-11 02:11:47 +0000

    add SystemState class

---
+++ hardware/app/state.py
@@ -0,0 +1,7 @@
+class SystemState:
+    def __init__(self):
+        self.mode = "motion"      # motion | manual
+        self.power = False
+        self.brightness = "medium"
+
+state = SystemState()
